65fbaa5ea87d241c3acb5f28b735c63e601eea1d
Tristan Scaglia <Scglia@users.noreply.github.com>
2022-12-05 20:49:10 +0100

    Add PodcastList

---
+++ components/podcast-list/PodcastList.tsx
@@ -0,0 +1,16 @@
+import { box } from "./PodcastList.css";
+import PodcastListItem from "./PodcastListItem";
+
+const PodcastList = ({ podcasts }: { podcasts: any }) => {
+  return (
+    <div className={box}>
+      {podcasts.map((item: any) => {
+        return (
+          <PodcastListItem imageUrl={item.imageUrl} podcastTitle={item.title} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default PodcastList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import "../styles/global.css";
-import PodcastListItem from "../components/podcast-list/PodcastListItem";
+import PodcastList from "../components/podcast-list/PodcastList";
 
 export default function Home() {
+  const podcasts = [
+    {
+      imageUrl:
+        "https://image.simplecastcdn.com/images/56e415f0-1911-44b3-9b1c-99551f7146c3/993ebed2-9e1d-4455-8d97-fa64f173572f/3000x3000/1471485006-artwork.jpg?aid=rss_feed",
+      title: "Design Details",
+    },
+    {
+      imageUrl:
+        "https://image.simplecastcdn.com/images/56e415f0-1911-44b3-9b1c-99551f7146c3/993ebed2-9e1d-4455-8d97-fa64f173572f/3000x3000/1471485006-artwork.jpg?aid=rss_feed",
+      title:
+        "Another Podcast With A Really Long Name By Sam Harris and Calvin Hobbes",
+    },
+  ];
+
   return (
     <div style={{ margin: "16px" }}>
-      <PodcastListItem
-        imageUrl="https://image.simplecastcdn.com/images/56e415f0-1911-44b3-9b1c-99551f7146c3/993ebed2-9e1d-4455-8d97-fa64f173572f/3000x3000/1471485006-artwork.jpg?aid=rss_feed"
-        podcastTitle="Design Details"
-      />
+      <PodcastList podcasts={podcasts} />
     </div>
   );
 }
