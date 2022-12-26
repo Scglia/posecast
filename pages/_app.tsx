a44390c579629da429e50bee9eb9b0c89f3c5914
Tristan Scaglia <Scglia@users.noreply.github.com>
2022-12-26 20:55:38 +0100

    Fix typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { Inter } from "@next/font/google";
-import PlayerLayout from "./app/playerLayout";
+import PlayerLayout from "./app/PlayerLayout";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["latin"] });
