27a09482c5fe0e55eaa8e07fb065ca397166d98b
Tristan Scaglia <Scglia@users.noreply.github.com>
2023-02-09 17:51:48 +0100

    Add examples on the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,45 @@
 import "../styles/global.css";
-import PodcastList from "../components/podcast-list/PodcastList";
-import Player from "../components/player/PlayerUI";
-import EpisodeListItem from "../components/episode-list/EpisodeListItem";
-import EpisodeList from "../components/episode-list/EpisodeList";
-import PodcastDetails from "../components/podcast-details/PodcastDetails";
-import Link from "next/link";
+import Button from "../components/generic/Button";
+import examplePodcasts from "../resources/data/podcastsData";
+import { regular } from "../styles/fonts.css";
+import usePodcastsStore from "../stores/podcastsStore";
 
 export default function Home() {
+  const addPodcast = usePodcastsStore((state: any) => state.addPodcast);
+
+  const addExemplePodcasts = () => {
+    Object.values(examplePodcasts).forEach((podcast) => {
+      addPodcast(podcast);
+    });
+  };
+
   return (
-    <div>
-      <Link href="/podcasts">Podcasts</Link>
+    <div
+      style={{
+        padding: "24px",
+        height: "100vh",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      <div>
+        <span style={{ color: "rgb(186, 186, 186)" }}>Welcome to posecast</span>{" "}
+        <br />
+        <span className={regular}> Listen to podcasts via RSS</span> <br />
+      </div>
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexGrow: 1,
+        }}
+      >
+        <Button href="/podcasts" onClick={addExemplePodcasts}>
+          Open the app with example podcasts
+        </Button>
+      </div>
     </div>
   );
 }
